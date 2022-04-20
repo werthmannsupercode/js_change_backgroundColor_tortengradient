fb96eefb973988afe71221820f2232b3db35fb80
werthmannsupercode <werthmannsupercode@gmail.com>
2022-04-20 15:46:59 +0200

    rgb renew

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,21 @@ let result = document.getElementById('result');
 let headLine = document.getElementById('headline');
 
 function colorOne() {
-    result.style.backgroundColor = "rgb(" + 59 + "," + 120 + "," + 76 + ")";
-    headLine.style.color = "rgb(" + 89 + "," + 19 + "," + 83 + ")";
+    result.style.backgroundColor = "rgb(59, 120, 76)";
+    headLine.style.color = "rgb(89, 19, 83)";
 }
 
 function colorTwo() {
-    result.style.backgroundColor = "rgb(" + 68 + "," + 66 + "," + 110 + ")";
-    headLine.style.color = "rgb(" + 210 + "," + 173 + "," + 85 + ")";
+    result.style.backgroundColor = "rgb(68, 66, 110)";
+    headLine.style.color = "rgb(210, 173, 85)";
 }
 
 function colorThree() {
-    result.style.backgroundColor = "rgb(" + 0 + "," + 0 + "," + 29 + ")";
-    headLine.style.color = "rgb(" + 185 + "," + 99 + "," + 39 + ")";
+    result.style.backgroundColor = "rgb(0, 0, 29)";
+    headLine.style.color = "rgb(185, 99, 39)";
 }
 
 function colorFour() {
-    result.style.backgroundColor = "rgb(" + 5 + "," + 4 + "," + 75 + ")";
-    headLine.style.color = "rgb(" + 95 + "," + 193 + "," + 239 + ")";
+    result.style.backgroundColor = "rgb(5, 4, 75)";
+    headLine.style.color = "rgb(95, 193, 239)";
 }
